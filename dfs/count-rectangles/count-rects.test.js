8bf835ca58be484af320965bd1201a22d5766042
IndieCoderMM <hthant00chk@gmail.com>
2023-10-07 00:19:23 +0630

    Add more tests

--- dfs/count-rectangles/count-rects.test.js
+++ dfs/count-rectangles/count-rects.test.js
@@ -1,8 +1,8 @@
-import countRectagles from './count-rects';
+import countRectangles from './count-rects';
 
 describe('Counting Rectangles', () => {
   it('should count all isolated rectangles', () => {
-    const array1 = [
+    const matrix1 = [
       [1, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
       [1, 0, 0, 1, 1, 1, 0, 0],
@@ -12,25 +12,58 @@ describe('Counting Rectangles', () => {
       [0, 0, 0, 1, 1, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 1, 0],
     ];
-    expect(countRectagles(array1)).toEqual(6);
+    expect(countRectangles(matrix1)).toEqual(6);
+
+    const matrix2 = [
+      [1, 0, 1, 0, 1],
+      [0, 1, 0, 1, 0],
+      [1, 0, 1, 0, 1],
+      [0, 1, 0, 1, 0],
+      [1, 0, 1, 0, 1],
+      [0, 1, 0, 1, 0],
+    ];
+    expect(countRectangles(matrix2)).toEqual(15);
+
+    const matrix3 = [
+      [1, 1, 1, 0, 1],
+      [1, 1, 1, 0, 0],
+      [1, 1, 1, 0, 1],
+      [1, 1, 1, 0, 0],
+      [1, 1, 1, 0, 1],
+      [0, 0, 0, 1, 0],
+      [1, 0, 1, 0, 1],
+    ];
+    expect(countRectangles(matrix3)).toEqual(8);
   });
 
-  it('should work for matrix with arbitary dimensions', () => {
-    const array2 = [
+  it('should work for irregular matrix with arbitary dimensions', () => {
+    const matrix1 = [
       [1, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
-      [1, 0, 0, 1, 1, 1, 0, 0],
+      [1, 0, 0, 1, 1, 1, 0],
       [0, 1, 0, 1, 1, 1, 0, 1, 1],
       [0, 1, 0, 0, 0, 0, 0, 0],
-      [0, 1, 0, 1, 1, 0, 0, 0],
+      [0, 1, 0, 1, 1, 0, 0],
       [0, 0, 0, 1, 1, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 1, 0, 1],
     ];
-    expect(countRectagles(array2)).toEqual(8);
+    expect(countRectangles(matrix1)).toEqual(8);
+
+    const matrix2 = [
+      [1, 0, 1],
+      [0, 1],
+      [1, 0, 1, 0],
+      [0],
+      [1, 0, 1, 0, 1, 0],
+      [0, 1],
+      [1],
+      [0, 1, 0],
+    ];
+    expect(countRectangles(matrix2)).toEqual(11);
   });
 
   it('should not count deformed shapes', () => {
-    const array3 = [
+    const matrix1 = [
       [1, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0],
       [1, 0, 0, 1, 1, 1, 0, 0],
@@ -40,6 +73,36 @@ describe('Counting Rectangles', () => {
       [0, 0, 0, 1, 1, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 1, 0],
     ];
-    expect(countRectagles(array3)).toEqual(5);
+    expect(countRectangles(matrix1)).toEqual(5);
+
+    const matrix2 = [
+      [1, 0, 0, 0, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 0, 0],
+      [1, 0, 0, 1, 1, 1, 1, 1, 1],
+      [1, 1, 0, 1, 1, 1, 0, 0, 1],
+      [0, 1, 0, 0, 0, 0, 0, 0, 1],
+      [0, 1, 1, 1, 1, 0, 0, 1, 1],
+      [0, 0, 0, 1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    ];
+    expect(countRectangles(matrix2)).toEqual(2);
+
+    const matrix3 = [
+      [1, 0, 0, 0, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 0, 0],
+      [1, 0, 0, 1, 1, 1, 1, 1, 0],
+      [1, 1, 0, 1, 1, 1, 0, 0, 0],
+      [0, 1, 0, 0, 0, 0, 0, 0, 0],
+      [0, 1, 0, 1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    expect(countRectangles(matrix3)).toEqual(1);
+  });
+
+  it('should handle edge case with a large matrix containing no rectangles', () => {
+    const largeMatrix = Array.from({ length: 100 }, () => Array(100).fill(0));
+    expect(countRectangles(largeMatrix)).toEqual(0);
   });
 });
